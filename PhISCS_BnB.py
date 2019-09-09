23d32104cd16e9d43cbf0a1d67377e5d2237ea0d
Farid Rashidi <farid.rsh@gmail.com>
2019-09-08 21:11:25 -0400

    update

--- PhISCS_BnB.py
+++ PhISCS_BnB.py
@@ -1,26 +1,35 @@
 import pybnb
 import numpy as np
-from utils import *
+# from utils import *
 import operator
 import datetime
 from collections import defaultdict
 from optparse import OptionParser
 parser = OptionParser()
-parser.add_option('-n', '--numberOfCells', dest='n', help='', type=int, default=30)
-parser.add_option('-m', '--numberOfMutations', dest='m', help='', type=int, default=15)
-parser.add_option('-p', '--partitionRandomly', dest='p', help='', action='store_true', default=False)
+parser.add_option('-n', '--numberOfCells', dest='n', help='', type=int, default=5)
+parser.add_option('-m', '--numberOfMutations', dest='m', help='', type=int, default=5)
+parser.add_option('-r', '--partitionRandomly', dest='r', help='', action='store_true', default=False)
+parser.add_option('-w', '--whichAlgorithm', dest='w', help='', type=str, default='b')
 options, args = parser.parse_args()
 
 
-noisy = np.random.randint(2, size=(options.n, options.m))
+# noisy = np.random.randint(2, size=(options.n, options.m))
+# print(noisy)
+noisy = np.array([
+    [0,0,1,0],
+    [1,0,1,1],
+    [1,1,1,1],
+    [0,1,0,1]
+])
+
 # ms_package_path = '/home/frashidi/software/bin/ms'
 # ground, noisy, (countFN,countFP,countNA) = get_data(n=30, m=15, seed=1, fn=0.20, fp=0, na=0, ms_package_path=ms_package_path)
-a = datetime.datetime.now()
-solution, (flips_0_1, flips_1_0, flips_2_0, flips_2_1) = PhISCS_I(noisy, beta=0.9999999, alpha=0.00000001)
-b = datetime.datetime.now()
-c = b - a
-print('PhISCS_I in microseconds: ', c.microseconds)
-print('Number of flips reported by PhISCS_I:', len(np.where(solution != noisy)[0]))
+# a = datetime.datetime.now()
+# solution, (flips_0_1, flips_1_0, flips_2_0, flips_2_1) = PhISCS_I(noisy, beta=0.9, alpha=0.00000001)
+# b = datetime.datetime.now()
+# c = b - a
+# print('PhISCS_I in microseconds: ', c.microseconds)
+# print('Number of flips reported by PhISCS_I:', len(np.where(solution != noisy)[0]))
 
 
 #––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
@@ -52,7 +61,7 @@ def is_conflict_free_gusfield_and_get_two_columns_in_coflicts(I):
             if O[i,j] == 1:
                 if Lij[i,j] != Lj[j]:
                     return False, (idx[j], idx[Lj[j]-1])
-    return True, None
+    return True, (None,None)
 
 def get_a_coflict(D, p, q):
     #todo: oneone is not important you can get rid of
@@ -167,7 +176,7 @@ class PhISCS_a(pybnb.Problem):
             return pybnb.Problem.infeasible_objective(self)
 
     def bound(self):
-        return self.flip + get_lower_bound(self.I, partition_randomly=options.p)
+        return self.flip + get_lower_bound(self.I, partition_randomly=options.r)
 
     def save_state(self, node):
         node.state = (self.I, self.idx, self.flip)
@@ -193,11 +202,11 @@ class PhISCS_a(pybnb.Problem):
             node.state = (I, self.idx+1, self.flip)
             yield node
 
+
 class PhISCS_b(pybnb.Problem):
     def __init__(self, I):
         self.I = I
-        self.X = np.zeros(I.shape, dtype=bool)
-        self.flip = 0
+        self.nflip = 0
     
     def sense(self):
         return pybnb.minimize
@@ -205,48 +214,146 @@ class PhISCS_b(pybnb.Problem):
     def objective(self):
         icf, _ = is_conflict_free_gusfield_and_get_two_columns_in_coflicts(self.I)
         if icf:
-            return self.flip
+            return self.nflip
         else:
             return pybnb.Problem.infeasible_objective(self)
 
     def bound(self):
-        return self.flip + get_lower_bound(self.I, partition_randomly=options.p)
+        return self.nflip + get_lower_bound(self.I, partition_randomly=options.r)
 
     def save_state(self, node):
-        node.state = (self.I, self.flip)
+        node.state = (self.I, self.nflip)
 
     def load_state(self, node):
-        self.I, self.flip = node.state
+        self.I, self.nflip = node.state
 
     def branch(self):
         icf, (p,q) = is_conflict_free_gusfield_and_get_two_columns_in_coflicts(self.I)
         p,q,oneone,zeroone,onezero = get_a_coflict(self.I, p, q)
         
-        if not self.X[onezero,q]:
-            self.X[onezero,q] = True
-            node = pybnb.Node()
-            I = self.I.copy()
-            I[onezero,q] = 1
-            node.state = (I, self.flip+1)
-            yield node
+        node = pybnb.Node()
+        I = self.I.copy()
+        I[onezero,q] = 1
+        node.state = (I, self.nflip+1)
+        yield node
         
-        if not self.X[zeroone,p]:
-            self.X[zeroone,p] = True
-            node = pybnb.Node()
-            I = self.I.copy()
-            I[zeroone,p] = 1
-            node.state = (I, self.flip+1)
-            yield node
+        node = pybnb.Node()
+        I = self.I.copy()
+        I[zeroone,p] = 1
+        node.state = (I, self.nflip+1)
+        yield node
 
 
+class PhISCS_c(pybnb.Problem):
+    def __init__(self, I):
+        self.I = I
+        self.nflip = 0
+        self.icf = None
+    
+    def sense(self):
+        return pybnb.minimize
 
-# problem = PhISCS_a(noisy)
-problem = PhISCS_b(noisy)
+    def objective(self):
+        if self.icf:
+            return self.nflip
+        else:
+            return pybnb.Problem.infeasible_objective(self)
+
+    def bound(self):
+        return self.nflip + get_lower_bound(self.I, partition_randomly=options.r)
+
+    def save_state(self, node):
+        node.state = (self.I, self.icf, self.nflip)
+
+    def load_state(self, node):
+        self.I, self.icf, self.nflip = node.state
+
+    def branch(self):
+        icf, (p,q) = is_conflict_free_gusfield_and_get_two_columns_in_coflicts(self.I)
+        if icf:
+            return
+        p,q,oneone,zeroone,onezero = get_a_coflict(self.I, p, q)
+        
+        node = pybnb.Node()
+        I = self.I.copy()
+        I[onezero,q] = 1
+        node.state = (I, icf, self.nflip+1)
+        yield node
+        
+        node = pybnb.Node()
+        I = self.I.copy()
+        I[zeroone,p] = 1
+        node.state = (I, icf, self.nflip+1)
+        yield node
+
+
+class PhISCS_d(pybnb.Problem):
+    def __init__(self, I):
+        self.I = I
+        self.nflip = 0
+        self.icf = None
+    
+    def sense(self):
+        return pybnb.minimize
+
+    def objective(self):
+        if self.icf:
+            return self.nflip
+        else:
+            return pybnb.Problem.infeasible_objective(self)
+
+    def bound(self):
+        return self.nflip + get_lower_bound(self.I, partition_randomly=options.r)
+
+    def save_state(self, node):
+        node.state = (self.icf, self.nflip)
+
+    def load_state(self, node):
+        self.icf, self.nflip = node.state
+
+    def branch(self):
+        icf, (p,q) = is_conflict_free_gusfield_and_get_two_columns_in_coflicts(self.I)
+        if icf:
+            return
+        p,q,oneone,zeroone,onezero = get_a_coflict(self.I, p, q)
+        
+        node = pybnb.Node()
+        self.I[onezero,q] = 1
+        self.icf = icf
+        node.state = (self.icf, self.nflip+1)
+        yield node
+        
+        self.I[onezero,q] = 0
+        
+        node = pybnb.Node()
+        self.I[zeroone,p] = 1
+        self.icf = icf
+        node.state = (self.icf, self.nflip+1)
+        yield node
+        
+        self.I[zeroone,p] = 0
+
+
+if options.w == 'a':
+    print('PhISCS_a is chosen')
+    problem = PhISCS_a(noisy)
+elif options.w == 'b':
+    print('PhISCS_b is chosen')
+    problem = PhISCS_b(noisy)
+elif options.w == 'c':
+    print('PhISCS_c is chosen')
+    problem = PhISCS_c(noisy)
+elif options.w == 'd':
+    print('PhISCS_d is chosen')
+    problem = PhISCS_d(noisy)
+else:
+    print('Wrong Algorithm')
 
 a = datetime.datetime.now()
-results = pybnb.solve(problem)
+results = pybnb.solve(problem, log_interval_seconds=10.0)
 b = datetime.datetime.now()
 c = b - a
 print('PhISCS_BnB in microseconds:', c.microseconds)
 print('Number of flips reported by PhISCS_BnB:', results.best_node.state[-1])
-print('Is the output matrix reported by PhISCS_BnB conflict free:', is_conflict_free_farid(results.best_node.state[0]))
+icf, _ = is_conflict_free_gusfield_and_get_two_columns_in_coflicts(results.best_node.state[0])
+print('Is the output matrix reported by PhISCS_BnB conflict free:', icf)
