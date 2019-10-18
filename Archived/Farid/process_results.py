3b935afbdda3d70c064e40db80def06cfee2725c
Farid Rashidi <farid.rsh@gmail.com>
2019-10-17 23:56:05 -0400

    update

--- Archived/Farid/process_results.py
+++ Archived/Farid/process_results.py
@@ -1,14 +1,29 @@
 import pandas as pd
+from glob import glob
+from collections import defaultdict
 
+kk_all = defaultdict(lambda: 0)
+kk_mwm = defaultdict(lambda: 0)
 
-def function(x):
-    i = x.query('method == "BnB_DynamicMWMBounding_None_False"')
-    j = x.query('method == "PhISCS_I_"')
-    print(x)
-    if not (i.empty or j.empty) and (i.runtime < j.runtime):
-        return x
+def get_counter(file):
+    df = pd.read_csv(file)
+    df = df[['hash', 'method', 'nf', 'runtime']]
+    counter = 0
+    for _, df_group in df.groupby("hash"):
+        k = df_group['nf'].values[0]
+        kk_all[k] += 1
+        mydict = pd.Series(df_group.runtime.values, index=df_group.method).to_dict()
+        if mydict['PhISCS_I_'] > mydict['BnB_DynamicMWMBounding_None_False']:
+            counter += 1
+            kk_mwm[k] += 1
+    return counter
 
+# file = "/data/frashidi/Phylogeny_BnB/reports/10,10_10-16-04-17-17.csv"
+# get_counter(file)
 
-file = "../reports/10,10_10-16-04-17-17.csv"
-df = pd.read_csv(file)
-print(df.groupby("hash", group_keys=False).apply(function))
+for file in glob('/data/frashidi/Phylogeny_BnB/reports/*10-16-*'):
+    get_counter(file)
+
+print(sorted(kk_all.items() ,  key=lambda x: x[0]))
+print()
+print(sorted(kk_mwm.items() ,  key=lambda x: x[0]))
