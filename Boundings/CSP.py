f2dd81916d79b04c29fa6fbbb85da1563f3909a8
Farid Rashidi <farid.rsh@gmail.com>
2019-10-19 15:17:23 -0400

    update

--- Boundings/CSP.py
+++ Boundings/CSP.py
@@ -2,60 +2,6 @@ from Utils.const import *
 from Utils.interfaces import *
 
 
-def PhISCS_B_helper(matrix):
-    rc2 = RC2(WCNF())
-    n, m = matrix.shape
-    fnWeight = 1
-    fpWeight = 10
-
-    Y = np.empty((n, m), dtype=np.int64)
-    numVarY = 0
-    mapY2ij = {}
-    for i in range(n):
-        for j in range(m):
-            numVarY += 1
-            mapY2ij[numVarY] = (i, j)
-            Y[i, j] = numVarY
-
-    X = np.empty((n, m), dtype=np.int64)
-    numVarX = 0
-    for i in range(n):
-        for j in range(m):
-            numVarX += 1
-            X[i, j] = numVarY + numVarX
-
-    B = np.empty((m, m, 2, 2), dtype=np.int64)
-    numVarB = 0
-    for p in range(m):
-        for q in range(m):
-            for i in range(2):
-                for j in range(2):
-                    numVarB += 1
-                    B[p, q, i, j] = numVarY + numVarX + numVarB
-
-    for i in range(n):
-        for j in range(m):
-            if matrix[i, j] == 0:
-                rc2.add_clause([-X[i, j]], weight=fnWeight)
-                rc2.add_clause([-X[i, j], Y[i, j]])
-                rc2.add_clause([X[i, j], -Y[i, j]])
-            elif matrix[i, j] == 1:
-                rc2.add_clause([-X[i, j]], weight=fpWeight)
-                rc2.add_clause([X[i, j], Y[i, j]])
-                rc2.add_clause([-X[i, j], -Y[i, j]])
-
-    for i in range(n):
-        for p in range(m):
-            for q in range(p, m):
-                rc2.add_clause([-Y[i, p], -Y[i, q], B[p, q, 1, 1]])
-                rc2.add_clause([Y[i, p], -Y[i, q], B[p, q, 0, 1]])
-                rc2.add_clause([-Y[i, p], Y[i, q], B[p, q, 1, 0]])
-                rc2.add_clause([-B[p, q, 0, 1], -B[p, q, 1, 0], -B[p, q, 1, 1]])
-
-    rc2.compute()
-    return rc2.cost
-
-
 class StaticCSPBounding(BoundingAlgAbstract):
     def __init__(self, split_into=2):
         self.matrix = None
@@ -71,13 +17,66 @@ class StaticCSPBounding(BoundingAlgAbstract):
     def get_name(self):
         return type(self).__name__ + "_" + str(self.split_into)
 
+    def _get_bound(self, matrix):
+        rc2 = RC2(WCNF())
+        n, m = matrix.shape
+        fnWeight = 1
+        fpWeight = 10
+
+        Y = np.empty((n, m), dtype=np.int64)
+        numVarY = 0
+        mapY2ij = {}
+        for i in range(n):
+            for j in range(m):
+                numVarY += 1
+                mapY2ij[numVarY] = (i, j)
+                Y[i, j] = numVarY
+
+        X = np.empty((n, m), dtype=np.int64)
+        numVarX = 0
+        for i in range(n):
+            for j in range(m):
+                numVarX += 1
+                X[i, j] = numVarY + numVarX
+
+        B = np.empty((m, m, 2, 2), dtype=np.int64)
+        numVarB = 0
+        for p in range(m):
+            for q in range(m):
+                for i in range(2):
+                    for j in range(2):
+                        numVarB += 1
+                        B[p, q, i, j] = numVarY + numVarX + numVarB
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i, j] == 0:
+                    rc2.add_clause([-X[i, j]], weight=fnWeight)
+                    rc2.add_clause([-X[i, j], Y[i, j]])
+                    rc2.add_clause([X[i, j], -Y[i, j]])
+                elif matrix[i, j] == 1:
+                    rc2.add_clause([-X[i, j]], weight=fpWeight)
+                    rc2.add_clause([X[i, j], Y[i, j]])
+                    rc2.add_clause([-X[i, j], -Y[i, j]])
+
+        for i in range(n):
+            for p in range(m):
+                for q in range(p, m):
+                    rc2.add_clause([-Y[i, p], -Y[i, q], B[p, q, 1, 1]])
+                    rc2.add_clause([Y[i, p], -Y[i, q], B[p, q, 0, 1]])
+                    rc2.add_clause([-Y[i, p], Y[i, q], B[p, q, 1, 0]])
+                    rc2.add_clause([-B[p, q, 0, 1], -B[p, q, 1, 0], -B[p, q, 1, 1]])
+
+        rc2.compute()
+        return rc2.cost
+
     def get_bound(self, delta):
         # https://stackoverflow.com/questions/16856788/slice-2d-array-into-smaller-2d-arrays
         bound = 0
         I = np.array(self.matrix + delta)
         blocks = np.array_split(I, self.split_into, axis=1)
         for block in blocks:
-            bound += PhISCS_B_helper(block)
+            bound += self._get_bound(block)
         return bound + delta.count_nonzero()
 
 
@@ -105,11 +104,6 @@ class SemiDynamicWCNFCSPBounding(BoundingAlgAbstract):
             self.yVars.append(yVar)
         model_time = time.time() - model_time
         self.times["model_preparation_time"] += model_time
-        # opt_time = time.time()
-        # for model in self.models:
-        #     variables = model.compute()
-        # opt_time = time.time() - opt_time
-        # self.times["optimization_time"] += opt_time
 
     def _get_blocks(self, I):
         return [I[:, i] for i in self.block_indices]
@@ -170,7 +164,6 @@ class SemiDynamicWCNFCSPBounding(BoundingAlgAbstract):
     def get_bound(self, delta):
         model_time = time.time()
         cx = delta.tocoo()
-        # print('-------------', cx.row, cx.col, cx.data)
         models = []
         for model in self.models:
             new_model = model.copy()
@@ -233,7 +226,7 @@ class SemiDynamicRC2CSPBounding(BoundingAlgAbstract):
         return [I[:, i] for i in self.block_indices]
 
     def _make_model(self, subMatrix):
-        rc2 = RC2(WCNF())
+        rc2 = RC2(WCNF(), solver="mc")  # mc, mgh, g3, g4, mcb, m22, mpl,
         n = subMatrix.shape[0]
         m = subMatrix.shape[1]
         fnWeight = 1
@@ -291,7 +284,7 @@ class SemiDynamicRC2CSPBounding(BoundingAlgAbstract):
         # print("-------------", cx.row, cx.col, cx.data)
         models = []
         for model in self.models:
-            new_model = copy.copy(model)
+            new_model = copy.deepcopy(model)
             models.append(new_model)
             # print(new_model)
         for i, j, v in zip(cx.row, cx.col, cx.data):
@@ -343,15 +336,19 @@ if __name__ == "__main__":
     print(algo1.get_bound(delta))
 
     algo2 = SemiDynamicRC2CSPBounding(4)
+    # algo2 = SemiDynamicWCNFCSPBounding(4)
     algo2.reset(noisy)
     print(algo2.get_bound(delta))
     print(algo2.times)
 
     delta[0, 0] = 1
     print(algo2.get_bound(delta))
+    print(algo2.times)
     delta[0, 5] = 1
     print(algo2.get_bound(delta))
+    print(algo2.times)
     delta[0, 9] = 1
     print(algo2.get_bound(delta))
+    print(algo2.times)
 
     print(algo1.get_bound(delta))
